9e6d17540ea87a43986b4217e0062ca02c410b55
Vlad <tsoiivv@gmail.com>
2023-04-20 23:59:05 +0600

    Added all items for the restaurant

--- app/src/main/java/com/example/map_app/RestFragment.kt
+++ app/src/main/java/com/example/map_app/RestFragment.kt
@@ -26,7 +26,12 @@ class RestFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val items: MutableList<Item> = mutableListOf()
-        items.add(Item("Cyclone", "ЧУЙ 136", R.drawable.a, "1.5km", "do 24 00"))
+        items.add(Item("Cyclone", "Проспект Чуй 136", R.drawable.a, "1.5 km", "Открыт до 24:00"))
+        items.add(Item("Anar", "ул. Иса Ахунбаева 17А", R.drawable.b, "1.5 km", "Открыт до 24:00"))
+        items.add(Item("Бай Казан", "Проспект  Чуй 136", R.drawable.c, "1.5 km", "Открыт до 24:00"))
+        items.add(Item("BARHAT", "Проспект Чуй 136", R.drawable.d, "1.5 km", "Открыт до 24:00"))
+        items.add(Item("Тринити", "Проспект Чуй 136", R.drawable.e, "1.5 km", "Открыт до 24:00"))
+        items.add(Item("Кайф", "Проспект Чуй 136", R.drawable.f, "1.5 km", "Открыт до 24:00"))
 
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = MyAdapter(requireContext(), items)
